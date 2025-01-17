6f2dfc501150ceed770f1e123bc648db9f760da1
Dreamboy-26 <heyimpankaj2616@gmail.com>
2025-01-17 15:44:27 +0530

    symbol change

--- Frontend/src/pages/Cart/Cart.jsx
+++ Frontend/src/pages/Cart/Cart.jsx
@@ -57,18 +57,18 @@ const Cart = () => {
           <div>
             <div className="cart-total-details">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p>&#8377;{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Delivery Fee</p>
-              <p>${getTotalCartAmount() === 0 ? 0 : 2}</p>
+              <p>&#8377;{getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
             <hr />
             <div className="cart-total-details">
               <p>Total</p>
               <p>
-                ${getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
+              &#8377;{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}
               </p>
             </div>
           </div>
